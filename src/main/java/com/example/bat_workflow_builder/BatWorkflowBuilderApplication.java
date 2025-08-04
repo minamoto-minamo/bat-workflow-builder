149338e6d953b23aa9d9db742b9916fb9ba8e0ac
minamo <hazime0401@gmail.com>
2025-08-04 23:55:18 +0900

    initial commit

---
+++ src/main/java/com/example/bat_workflow_builder/BatWorkflowBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.bat_workflow_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatWorkflowBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatWorkflowBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bat_workflow_builder/BatWorkflowBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bat_workflow_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatWorkflowBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
